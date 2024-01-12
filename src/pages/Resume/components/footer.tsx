5c3a34235f1b46217a96c86fc9dae92001210de9
Baday19 <1556330234@qq.com>
2024-01-12 18:08:52 +0800

    feat: submit

--- src/pages/Resume/components/footer.tsx
+++ src/pages/Resume/components/footer.tsx
@@ -6,6 +6,7 @@ export const Footer = () => {
   return (
     <div className="opcv-footer">
       <div>made with 🥰 by gooday</div>
+      <a href="https://github.com/baday19/OnePageCV">github</a>
       {/* <div>have a good day!</div> */}
     </div>
   )
